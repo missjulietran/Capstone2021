f90ece27a1dc5047d7aa4fcc798352adba0cd5f7
missjulietran <76934982+missjulietran@users.noreply.github.com>
2021-04-27 22:50:39 +0800

    Navbar1 and 2 done - all pages work

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Navbar2 from './components/Navbar2';
+import Navbar from './components/Navbar1/NavBar';
+import Navbar2 from './components/Navbar2/NavBar2';
 import './App.css';
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import LogIn from './components/pages/LogIn';
-import SignUp from './components/pages/SignUp';
-import Marketing from './components/pages/Marketing';
-import Consulting from './components/pages/Consulting';
+import { Home } from "./components/pages/Home";
+import { About } from "./components/pages/About";
+import { Cart } from "./components/pages/Cart";
+import { Contact } from "./components/pages/Contact";
 import Footer from './components/Footer';
-import { GlobalStyle } from './globalStyles';
+import HomePage from "./components/HomePage";
+// import {Events} from "./components/pages/Events";
+import {Brands} from "./components/pages/Brands";
+import {Categories} from './components/pages/Categories'
+import {SignUp} from './components/pages/SignUp'
+import {LogIn} from "./components/pages/LogIn";
+
+
+
 
 
 function App() {
   return (
     <Router>
-      <GlobalStyle />
       <Navbar />
       <Navbar2 />
+
+      <Route path='/' exact component={Home} />
+      {/* <HomePage />  */}
       <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/services' component={Services} />
-        <Route path='/products' component={Products} />
-        <Route path='/log-in' component={LogIn} />
-        <Route path='/sign-up' component={SignUp} />
-        <Route path='/marketing' component={Marketing} />
-        <Route path='/consulting' component={Consulting} />
+      <Route path="/LogIn" component={LogIn} />
+      <Route path="/SignUp" component={SignUp} />
+      {/* <Route path="/Events" component={Events} /> */}
+      <Route path="/Brands" component={Brands} />
+      <Route path="/Categories" component={Categories} />
+        <Route path="/About" component={About} />
+        <Route path="/Cart" component={Cart} />
+        <Route path="/Contact" component={Contact} />
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import '../../App.css';
+import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+// import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
 
-export default function Home() {
-  return (
-    <>
-      <h1 className='home'>Main Landing page</h1>
-    </>
-  );
+export const Home = () => {
+    return (
+        <div>
+            <Jumbotron>
+  <h1>Future Jumbotron</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
+                    
+                    <Container>
+                        <Row>
+                    <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="src/images/img-1.jpg" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Row>
+        </Container>
+        </div>
+    )
+
+    
 }
+
+export default Home;

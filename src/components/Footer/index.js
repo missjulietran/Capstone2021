25a845f3049a698c377f951ac70ae06e14757f4a
kimberlychan1 <76764912+kimberlychan1@users.noreply.github.com>
2021-05-13 18:11:52 +0800

    comment out unused item

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,9 @@
 // import React from 'react';
 // import "./Footer.css"
-import "./Footer.css";
 import { Link } from "react-router-dom";
 import React, { Component } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import styles from "./Footer.module.css"
+// import styles from "./Footer.module.css";
 
 const stickyFooterStyle = {
   backgroundColor: "#ffffff",
